987ac9a42b73cf2395393d903bbaebeb5e7d908a
haochen <1404276788@qq.com>
2019-06-20 08:58:43 +0800

    1

--- py_get_html.py
+++ py_get_html.py
@@ -4,9 +4,9 @@ import time
 
 session = HTMLSession()
 
-url_list=["http://www.atzhuan.com/uploads/160406/web.php?total=10","http://www.guhuituan.com/uploads/170622/web.php?total=10","http://www.5252yue.com/uploads/140607/web.php?total=10","http://qicaipendu.com/uploads/141220/web.php?total=10","http://www.yzxlp.com/uploads/170622/web.php?total=10","http://www.lyrjpx.com/uploads/170622/web.php?total=10","http://xincgy.com/uploads/allimg/130118/web.php?total=10","http://aiche1.com/uploads/140607/web.php?total=10","http://klmlct.com/uploads/allimg/140826/web.php?total=10","http://www.hbcckt.com/uploads/170622/web.php?total=10","http://ywlhjn.com/uploads/flink/web.php?total=10","http://www.dlqinghong.com/uploads/160406/web.php?total=10","http://www.0512mt.com/uploads/160406/web.php?total=10","http://0512mt.com/uploads/media/web.php?total=10","http://pylcmy.com/uploads/121024/web.php?total=10"]
+url_list=["http://www.nongzhile.com/uploads/soft/web.php?total=10","http://bcsrx.com/uploads/140604/web.php?total=10","http://www.clubhna.com/uploads/170622/web.php?total=10","http://www.syky958.com/uploads/160406/web.php?total=10","http://www.ynzjty.com/a/guanyu/web.php?total=10","http://www.jyspe.com/uploads/140607/web.php?total=10","http://0574mg.com/a/bywh/web.php?total=10","http://jxyns.com/images/web.php?total=10","http://www.92yuepa.com/uploads/160406/web.php?total=10","http://yize58.com/uploads/litimg/web.php?total=10","http://www.weiai52.com/a/yule/web.php?total=10","http://www.fh8edd.com/uploads/allimg/web.php?total=10","http://www.575yx.com/uploads/allimg/c170622/web.php?total=10","http://znkhd.com/uploads/allimg/160109/web.php?total=10","http://xiaonitv.com/uploads/150302/web.php?total=10","http://hjfw.net/uploads/userup/web.php?total=10","http://www.997db.com/uploads/allimg/170622/web.php?total=10","http://www.wodcq.com/uploads/allimg/140504/web.php?total=10","http://changsir.cn/wordpress/wp-content/uploads/2017/web.php?total=10","http://changsir.cn/wordpress/wp-content/uploads/2017/web.php?total=10","http://fast.watch.kct99.com/assets/addons/crontab/web.php?total=10","http://watch.kct99.com/assets/19d0a61b/web.php?total=10","t1.kct99.com/assets/354d7562/web.php?total=10"]
 
-i=15 #循环次数
+i=30 #循环次数
 for j in range(i):
     for url in url_list:
         try:
