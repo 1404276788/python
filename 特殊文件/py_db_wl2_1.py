02c4e683992434a8775e2c6e5305d8e9e334bc2c
haochen <1404276788@qq.com>
2019-09-17 15:36:17 +0800

    1

--- 特殊文件/py_db_wl2_1.py
+++ 特殊文件/py_db_wl2_1.py
@@ -2,7 +2,7 @@
 #动态寄生虫每个路径后面随机拼接字符串，生成20个不同的路径，保存到指定文件中
 import shutil
 import os
-import pymysql
+# import pymysql
 import re
 import random
 import string
@@ -13,15 +13,16 @@ def zz(zstr,s,stxt):
     url_q=strinfo.sub(s,stxt)
     return url_q
 
-file_name="C:/Users/Administrator/Desktop/abc.txt" #读文件
+file_name="abc.txt" #读文件
 
 for line in open(file_name,encoding='utf-8'):    
     if line:
         i=1
         while i<=20:
-            a=''.join(random.sample(string.ascii_letters + string.digits, 10)) #ascii_letters 字母 digits 数字 随机生成10位
+            #a=''.join(random.sample(string.ascii_letters + string.digits, 10)) #ascii_letters 字母 digits 数字 随机生成10位
+            a=''.join(random.sample(string.digits, 10)) #ascii_letters 字母 digits 数字 随机生成10位
             s=line.strip('\n')+'?'+a+".html"  #line.strip('\n') 去除换行
-            f=open('C:/Users/Administrator/Desktop/s1/1.txt','a',encoding='utf-8') #写文件
+            f=open('1.txt','a',encoding='utf-8') #写文件
             f.write(s+'\n')
             f.close()
             i+=1
