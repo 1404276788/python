cbc38da1df470479ed8ce0ae204d94c34a569af4
chenhao <1404276788@qq.com>
2019-09-28 16:53:25 +0800

    1

--- html_hanzi.py
+++ html_hanzi.py
@@ -1,10 +1,18 @@
-a='中国'
-un=ascii(a)
-un=un.replace('\'','')
-print(str(un))
-unlist=un.split('\\')
-unlist.remove(unlist[0])
-for unstr in unlist:
-    unstr='&#x'+unstr+';'
-    print(unstr)
 
+
+def strcoding(source_string):
+    un = ascii(source_string)
+    un = un.replace('\'','')
+    un = un.replace('u','')
+    unlist = un.split('\\')
+    unlist.remove(unlist[0])
+    print(unlist)
+    str_l = ''
+    for unstr in unlist:
+        unstr=unstr.upper()
+        unstr ='&#x'+unstr+';'
+        str_l = str_l + unstr
+    return str_l
+
+
+print(strcoding('中国代孕'))
